6a5386a6a66772dabe21942c906022272a2468e2
June0619 <jjw5050@hotmail.com>
2020-01-10 13:25:30 +0900

    HelloController test code

---
+++ src/main/java/com/buildsomething/zs/springboot/HelloController.java
@@ -0,0 +1,16 @@
+package com.buildsomething.zs.springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+// JSON을 반환하는 컨트롤러 생성
+public class HelloController
+{
+
+    @GetMapping("/hello")
+    public String hello()
+    {
+        return "hello";
+    }
+}
