671925f4fe67ad1611d81c4fc8ac8d3c0b7d786a
June0619 <jjw5050@hotmail.com>
2020-01-10 14:51:21 +0900

    HelloResponseDTO & test case

---
+++ src/main/java/com/buildsomething/zs/springboot/web/dto/HelloResponseDTO.java
@@ -0,0 +1,12 @@
+package com.buildsomething.zs.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDTO
+{
+    private final String name;
+    private final int amount;
+}
