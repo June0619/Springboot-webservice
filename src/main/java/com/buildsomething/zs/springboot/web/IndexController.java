371b512fb3c7dfb4b7975f527d799d241b5fd4bf
June0619 <jjw5050@hotmail.com>
2020-01-20 13:32:32 +0900

    Session Annotation refactoring

--- src/main/java/com/buildsomething/zs/springboot/web/IndexController.java
+++ src/main/java/com/buildsomething/zs/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.buildsomething.zs.springboot.web;
 
+import com.buildsomething.zs.springboot.config.auth.LoginUser;
 import com.buildsomething.zs.springboot.config.auth.dto.SessionUser;
 import com.buildsomething.zs.springboot.domain.user.User;
 import com.buildsomething.zs.springboot.service.posts.PostsService;
@@ -20,10 +21,9 @@ public class IndexController
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model)
+    public String index(Model model, @LoginUser SessionUser user)
     {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if (user != null)
             model.addAttribute("userName", user.getName());
